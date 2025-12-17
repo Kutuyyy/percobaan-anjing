3d340aea3629d2e5f725fab48cf17de4a265cfd2
Kutuy <febry.gundar@gmail.com>
2025-12-17 18:45:38 +0700

    Update and rename percobaan11.lua to percobaan12.lua

--- percobaan12.lua
+++ percobaan12.lua
@@ -1536,24 +1536,7 @@ local function createMainUI()
             SideBarWidth = 180,
             HasOutline = true,
         })
-        if Window.EditOpenButton then
-            pcall(function()
-                Window:EditOpenButton({
-                    Title = "Papi Dimz |HUB",
-                    Icon = "sparkles",
-                    CornerRadius = UDim.new(0, 16),
-                    StrokeThickness = 2,
-                    Color = ColorSequence.new(
-                        Color3.fromRGB(255, 15, 123),
-                        Color3.fromRGB(248, 155, 41)
-                    ),
-                    OnlyMobile = true,
-                    Enabled = true,
-                    Draggable = true,
-                })
-            end)
-        end
-
+        
         mainTab = Window:Tab({ Title = "Main", Icon = "settings-2" })
         localTab = Window:Tab({ Title = "Local Player", Icon = "user" })
         fishingTab = Window:Tab({ Title = "Fishing", Icon = "fish" })
